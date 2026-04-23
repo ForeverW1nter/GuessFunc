1a4a63700d58d7376f722bd9f6ad9088724aecbf
ForeverW1nter <ForeverW1nter@users.noreply.github.com>
2026-04-23 08:09:12 +0000

    feat: 移动分支内容到_old文件夹
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- __tests__/setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom'
-
-// Mock ResizeObserver for JSDOM
-class ResizeObserverMock {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
-globalThis.ResizeObserver = ResizeObserverMock
